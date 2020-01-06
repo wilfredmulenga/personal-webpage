69084eea8ed30da5618a90f56e13a44d2bc74166
wilfredmulenga <wilfredmulenga@yahoo.com>
2020-01-06 22:50:38 +0200

    refactor: alt attribute for image tag

--- src/ProjectItem.js
+++ src/ProjectItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectItem = ({ title, description, link, img }) => {
   return (
     <div className="card">
-      <img className="project-item-image" src={img} alt="project-item-image"/>
+      <img className="project-item-image" src={img} alt="project-item"/>
       <div className="text-container">
         <p className="title">{title}</p>
         <p className="description">{description}</p>
